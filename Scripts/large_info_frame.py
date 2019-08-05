ce8b7faf95fdc87d152e0cd9c561b875fef2d228
JarnoVos <jarno908@hotmail.com>
2019-08-05 02:24:14 +0200

    Adjusted window sizes
    Changed documents_frame width
    Changed large_info_frame width

--- Scripts/large_info_frame.py
+++ Scripts/large_info_frame.py
@@ -12,5 +12,5 @@ def InfoFrame(parent, info):
     i = 0
     for key, value in info.items():
         ttk.Label(parent, text=key).grid(row=i, column=0, padx=5)
-        ttk.Label(parent, text=value, width=60, wraplength=500).grid(row=i, column=1, sticky="w", pady=10)
+        ttk.Label(parent, text=value, width=80, wraplength=500).grid(row=i, column=1, sticky="w", pady=10)
         i += 1
